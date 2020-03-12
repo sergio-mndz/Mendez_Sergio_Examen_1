adba4f66f68d8ca5e83ef25d0f300ed861cf59b5
Sergio Mendez <39496193+sergio-mndz@users.noreply.github.com>
2020-03-12 17:06:25 -0600

    Added pit and sw

--- main.c
+++ main.c
@@ -17,7 +17,15 @@ gpio_pin_control_register_t led_enable = GPIO_MUX1;
 gpio_pin_control_register_t sw_enable = GPIO_MUX1 | GPIO_PE | GPIO_PS |
 										INTR_FALLING_EDGE;
 
+uint8_t g_timer_end_flag = FALSE;
+
+void pit_handler(void)
+{
+	g_timer_end_flag = TRUE;
+}
+
 int main(void) {
+
 
 	void (*led_turn[5])();
 
@@ -27,12 +35,16 @@ int main(void) {
 	led_turn[3] = purple_on;
 	led_turn[4] = cyan_on;
 
+	GPIO_clock_gating(GPIO_A);
 	GPIO_clock_gating(GPIO_B);
+	GPIO_clock_gating(GPIO_C);
 	GPIO_clock_gating(GPIO_E);
 
 	GPIO_pin_control_register(GPIO_B, bit_21, & led_enable);
 	GPIO_pin_control_register(GPIO_B, bit_22, & led_enable);
 	GPIO_pin_control_register(GPIO_E, bit_26, & led_enable);
+	GPIO_pin_control_register(GPIO_A, bit_4, & sw_enable);
+	GPIO_pin_control_register(GPIO_C, bit_6, & sw_enable);
 
 	red_off();
 	blue_off();
@@ -41,4 +53,26 @@ int main(void) {
 	GPIO_data_direction_pin(GPIO_B, GPIO_OUTPUT, bit_21);
 	GPIO_data_direction_pin(GPIO_B, GPIO_OUTPUT, bit_22);
 	GPIO_data_direction_pin(GPIO_E, GPIO_OUTPUT, bit_26);
+
+	GPIO_data_direction_pin(GPIO_A, GPIO_INPUT, bit_4);
+	GPIO_data_direction_pin(GPIO_C, GPIO_INPUT, bit_6);
+
+	NVIC_enable_interrupt_and_priotity(PORTA_IRQ,PRIORITY_5);
+	NVIC_enable_interrupt_and_priotity(PORTC_IRQ,PRIORITY_5);
+
+	PIT_clock_gating();
+
+	PIT_delay(PIT_0, SYSTEM_CLOCK, DELAY);
+
+	PIT_enable();
+
+	uint8_t init_counter = 0;
+	uint8_t led_sw2_counter = 0;
+
+	while(1){
+		if(TRUE == GPIO_get_irq_status(GPIO_A)){
+
+		}
+
+	}
 }
